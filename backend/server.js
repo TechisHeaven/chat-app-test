2419e76b73b56abbc7e2ed324839c266b7810d64
Himanshu <vermajihimanshu2004@gmail.com>
2023-06-29 18:33:03 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,9 @@ const ConnectDB = require("./config/db");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
+const cors = require("cors");
+
+app.use(cors());
 dotenv.config();
 ConnectDB();
 
